36fb2a5566e0ea91e7d904bca1910b6e3680f580
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-08-16 07:51:35 +0100

    Update home.js

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import { TextField, Stack, Button } from '@mui/material';
 import { useForm } from 'react-hook-form';
+import { TextField, Stack, Button } from '@mui/material';
+
 const Home = () => {
+  const form = useForm();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = form;
   return (
     <div>
-      <from>
+      <form>
         <Stack spacing={2} width={400}>
-          <TextField label="Enter task" variant="outline" />
+          <TextField
+            label="Enter task"
+            variant="outline"
+            type="text"
+            {...register('task')}
+          />
           <Button variant="contained">Add</Button>
         </Stack>
-      </from>
+      </form>
     </div>
   );
 };
